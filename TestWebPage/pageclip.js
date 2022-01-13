75d6711e33a11f822cf843e3c685f17dc0fda915
20120364 <phamphuocsang2834@gmail.com>
2022-01-13 19:27:45 +0700

    Tét

--- TestWebPage/pageclip.js
+++ TestWebPage/pageclip.js
@@ -4,7 +4,7 @@
     var e = "string" == typeof __pageclip_base ? __pageclip_base : "https://send.pageclip.co",
       m = {
         _base: e,
-        _successTemplate: '<div class="checkmark-container"><svg x="0px" y="0px" fill="none" class="checkmark-svg" viewBox="0 0 25 30"><path d="M2,19.2C5.9,23.6,9.4,28,9.4,28L23,2"/> </svg></div>',
+        _successTemplate: '<div class="pageclip-form__success__message">Thank you!</div>',
         cssBase: "pageclip-form",
         form: function(n, r) {
        
