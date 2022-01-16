cdd0e35de0690a1c68320b2a47acac99a541e59a
20120364 <phamphuocsang2834@gmail.com>
2022-01-16 16:40:45 +0700

    Finish 1

---
+++ TestWebPage/script.js
@@ -0,0 +1,271 @@
+$(document).ready(function() {
+    let summarydata=0
+    let ProfilePerPage=5;
+    let thepage=1;
+   
+    $(function()
+    {
+      var pageline=" <a id='prev' href='#prev' >&laquo;</a>"
+      $(pageline).appendTo("#pagechange")
+      $.getJSON('data.json', function(data) {
+           $.each(data.person, function(i, f) {
+            if(Math.floor(i/5+1)===(i/5)+1)
+            {
+              pageline=" <a "+ "id='thepage"+Math.floor(i/5+1).toString()+"' class='pagebutton-show' href='#pagenum"+Math.floor(i/5+1).toString()+"'>"+Math.floor(i/5+1).toString()+"</a>"
+              $(pageline).appendTo("#pagechange")   
+              $('#thepage'+thepage.toString()).addClass("active");  
+            }
+               
+         });  
+         pageline="<a id='next' href='#next' >&raquo;</a>"
+         $(pageline).appendTo("#pagechange")
+       });
+   
+     
+       
+    });
+    
+    function searchFunction() {
+    // Declare variables
+    var input, filter, ul, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementsByClassName("zoom item");
+  
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i <ul.length; i++) {
+      a = ul[i].getElementsByTagName('span')[0];
+     
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        ul[i].style.display = "";
+      } else {
+        ul[i].style.display = "none";
+      }
+    }
+  }
+        $(function() {
+    
+    
+       var people = [];
+        let count=0;
+        let countdv=0;
+        var display_image=""
+       $.getJSON('data.json', function(data) {
+           $.each(data.person, function(i, f) {
+              console.log(Math.floor(i/5+1))
+                if(Math.floor(i/5+1)===(i/5)+1 && Math.floor(i/5+1)!=1)
+                {
+              var display_image="<div id='pagenum"+Math.floor(i/5+1).toString()+"' class ='page-inactive'>  </div>"
+              $(display_image).appendTo("#userimg");
+                }
+                else{
+                  if(Math.floor(i/5+1)===(i/5)+1 && Math.floor(i/5+1)===1)
+                  {
+                var display_image="<div id='pagenum"+Math.floor(i/5+1).toString()+"' class ='page-active'>  </div>"
+                $(display_image).appendTo("#userimg");
+                  }
+                }
+                
+              })
+            });
+                $.getJSON('data.json', function(data) {
+                  $.each(data.person, function(i, f) {    
+                
+                    var display_image="<div class ='zoom item col ' id='people"+Math.floor(i/5+1).toString()+"'> <img class='profile-picture'"+"id='image"+i.toString()+"'"+" src= '"+f.image+"' width=150 height=100>"
+                    +"<span> "+f.firstName+" "+f.lastName+ "</span> </div> "
+                    $(display_image).appendTo("div > #pagenum"+Math.floor(i/5+1).toString());
+              
+              var display_info= "<div class ='info-nonactive'id='info"+i.toString()+"'><ul class='info-display caption'> "+"<li><p>"+f.firstName+" "+f.lastName+"</p></li>"+ "<li> <p>"  + f.job + "</li> </p>" +"<li> <p> " + f.roll + "</li> </p>" + " </ul></div>"
+               
+                $(display_info).appendTo("#user-info-display")
+            
+               summarydata=summarydata+1;
+               
+         })})});
+   
+   
+            // document.getElementById('pagepinright').addEventListener("click", function(event) {
+            //     if(thepage+1<numberOfPage)
+            //     {
+            //       thepage=thepage+1;
+            //     }
+            //     event.preventDefault();
+            // });
+            // document.getElementById('pagepinleft').addEventListener("click", function(event) {
+            //     if(thepage-1>0)
+            //     {
+            //       thepage=thepage-1;
+            //     }
+            //     event.preventDefault();
+            // });
+          
+//            setInterval(function()
+//             {
+//               var numberOfPage=Math.floor(summarydata/ProfilePerPage);
+// var countting=1;
+// console.log("thepage: "+thepage.toString())
+// for (var page=1;page<numberOfPage+1;page++) 
+// {
+ 
+//     if(page==thepage ||page==thepage+1 || page==thepage-1)
+//    {
+//      $('#thepage'+page.toString()).removeClass("pagebutton-hidden")
+//      $('#thepage'+page.toString()).addClass("pagebutton-show")
+//      if(page == thepage)
+//      {
+//        $('#thepage'+page.toString()).addClass("active");
+//      }
+//      else{
+       
+    
+//        if( document.getElementById('thepage'+page.toString())!=null)
+//         {
+        
+//         document.getElementById('thepage'+page.toString()).addEventListener("click", function(event) {
+//           thepage=countting -thepage
+//           event.preventDefault()
+//        });
+    
+//      }
+//        $('#thepage'+page.toString()).removeClass("active");
+//      }
+//    }
+//    else{
+//      $('#thepage'+page.toString()).addClass("pagebutton-hidden")
+//      $('#thepage'+page.toString()).removeClass("pagebutton-show")
+//      $('#thepage'+page.toString()).removeClass("active");
+//    }
+//    countting=countting+1;
+
+// }
+// for (var itemshow=0;itemshow<summarydata;itemshow++) 
+// {
+//    if(itemshow>=(thepage-1)*ProfilePerPage && itemshow<thepage*ProfilePerPage)
+//    {
+//      $('#people'+itemshow.toString()).removeClass("hide-item");
+//    }
+//    else{
+//      $('#people'+itemshow.toString()).addClass("hide-item");
+//    }
+
+// }
+
+//             }
+//             ,1000)
+      
+    setInterval(
+    function() {
+      
+   
+    var people = [];
+     let count=0;
+   
+    
+    $.getJSON('data.json', function(data) {
+        $.each(data.person, function(i, f) {
+              
+           
+               var check_hover="#image"+count.toString();
+               var Id_check="#info"+count.toString();
+               if($(check_hover).is(":hover") === true) 
+               {
+                $(Id_check).removeClass("info-nonactive");
+                  $(Id_check).addClass("info-active");
+                  var elementID=document.getElementById("image"+count.toString())
+                
+                  if(elementID!=null)
+                 {
+                  let width = screen.width;
+                  var bodyRect = document.body.getBoundingClientRect()
+                    var picpositionLeft=elementID.getBoundingClientRect().left - bodyRect.left
+                    var picpositionTop=elementID.getBoundingClientRect().top - bodyRect.top
+                    var picpositionRight=elementID.getBoundingClientRect().right - bodyRect.right
+                   
+                    if(picpositionLeft+200+300<width)
+                    {
+                  document.getElementById("info"+count.toString()).style.left=picpositionLeft+200+"px";
+                    }
+                    else{
+                      document.getElementById("info"+count.toString()).style.left=picpositionLeft-310+"px";
+                    }
+                  document.getElementById("info"+count.toString()).style.top=picpositionTop+'px';
+                 }
+               }
+               else{
+                $(Id_check).removeClass("info-active");
+                $(Id_check).addClass("info-nonactive");
+            
+                 
+               }
+        
+               count=count+1;
+      });  
+ 
+    });
+ 
+ });
+
+ window.onload=function()
+ {
+ var pageItem = $(".pagination a").not("#prev, #next");
+        
+  pageItem.click(function() {
+    
+    pageItem.removeClass("active");
+    $(this).not("#prev, #next").addClass("active");
+
+    target = $(this).attr('href');
+  
+    $('div '+target).removeClass('page-inactive');
+    $('div '+target).addClass('page-active');
+    $('#userimg > div:not('+target+')').removeClass('page-active');
+    $('#userimg > div:not('+target+')').addClass('page-inactive');
+    $(target).fadeIn(600);
+  });
+
+  $("#prev").click(function() {
+    var bar= $('a.active')
+    bar.removeClass('active')
+    var car=bar.prev()
+    car.addClass('active');
+    var laf =$('a.active').attr('href')
+    console.log(laf)
+    if(laf!='#prev')
+    {
+      $('div '+laf).removeClass('page-inactive');
+      $('div '+laf).addClass('page-active');
+      $('#userimg > div:not('+laf+')').removeClass('page-active');
+      $('#userimg > div:not('+laf+')').addClass('page-inactive');
+      $(laf).fadeIn(600);
+    }
+    if ($(this).hasClass("active"))
+    {
+      $(this).removeClass('active').next().addClass('active');
+      
+    }
+  });
+
+  $("#next").click(function() {
+    $('a.active').removeClass('active').next().addClass('active');
+    var laf =$('a.active').attr('href')
+    if(laf!='#next')
+    {
+      $('div '+laf).removeClass('page-inactive');
+      $('div '+laf).addClass('page-active');
+      $('#userimg > div:not('+laf+')').removeClass('page-active');
+      $('#userimg > div:not('+laf+')').addClass('page-inactive');
+      $(laf).fadeIn(600);
+    }
+    if ($(this).hasClass("active"))
+
+    {
+      $(this).removeClass('active').prev().addClass('active');
+    
+   
+    }
+  });
+ }
+   
+  });
